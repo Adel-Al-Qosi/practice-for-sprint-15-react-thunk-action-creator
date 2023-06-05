c4b44774d2f6196ed7ef18164f389bbd34637538
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-06-05 21:42:43 +0300

    finish practice

--- frontend/src/store/articleReducer.js
+++ frontend/src/store/articleReducer.js
@@ -1,9 +1,7 @@
-import articles from '../data/data.json';
-
 const LOAD_ARTICLES = 'article/loadArticles';
 const ADD_ARTICLE = 'article/addArticle';
 
-export const loadArticles = () => {
+export const loadArticles = (articles) => {
   return {
     type: LOAD_ARTICLES,
     articles
@@ -17,12 +15,33 @@ export const addArticle = (article) => {
   };
 };
 
+export const fetchArticles = () => async (dispatch) => {
+  const res = await fetch('/api/articles');
+  const articles = await res.json();
+  dispatch(loadArticles(articles));
+};
+
+export const writeArticle = (payload) => async (dispatch) => {
+  const res = await fetch('/api/articles', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+  });
+  const article = await res.json();
+  dispatch(addArticle(article));
+};
+
 const initialState = { entries: [], isLoading: true };
 
 const articleReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_ARTICLES:
-      return { ...state, entries: [...action.articles] };
+      if (Array.isArray(action.articles)) {
+        return { ...state, entries: [...action.articles] };
+      }
+      return state;
     case ADD_ARTICLE:
       return { ...state, entries: [...state.entries, action.article] };
     default:
